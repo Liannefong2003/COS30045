b6849fe217d1c34c4a90f5dd656084bf881e0cf2
Liannefong <liannefong2003@gmail.com>
2024-09-03 08:33:03 +0800

    Initial commit

---
+++ Lab1/Lab1.js
@@ -0,0 +1,5 @@
+function show (src, des,alter){
+    document.getElementById('images').src = src ;
+    document.getElementById('description').textContent = des;
+    document.getElementById('images').alt =alter;
+}
